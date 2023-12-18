54b7a6634efdb6d43be639525ec8e1ddc92a7288
ozge belen <ozgebelen@ozge-MacBook-Pro.local>
2023-12-19 00:02:31 +0300

    drawer wiew fixed again

--- lib/theme/tobeto_theme.dart
+++ lib/theme/tobeto_theme.dart
@@ -13,11 +13,18 @@ class TobetoAppTheme {
         ),
         colorScheme: TobetoAppColor.colorSchemeLight,
         textTheme: GoogleFonts.poppinsTextTheme().copyWith(
-            labelLarge: GoogleFonts.poppins(
-                color: TobetoAppColor.backgroundDark,
-                fontWeight: FontWeight.bold),
-            bodyMedium: TextStyle(
-                color: TobetoAppColor.textColor, fontWeight: FontWeight.bold)),
+          labelLarge: GoogleFonts.poppins(
+              color: TobetoAppColor.backgroundDark,
+              fontWeight: FontWeight.bold),
+          bodyMedium: TextStyle(
+              color: TobetoAppColor.textColor,
+              fontWeight: FontWeight.bold,
+              fontSize: 18),
+          bodySmall: TextStyle(
+            fontSize: 16,
+            color: TobetoAppColor.textColor,
+          ),
+        ),
         inputDecorationTheme: InputDecorationTheme(
           prefixIconColor: TobetoAppColor.textColor,
           suffixIconColor: TobetoAppColor.textColor,
@@ -32,39 +39,50 @@ class TobetoAppTheme {
               foregroundColor: TobetoAppColor.textColorDark),
         ),
         textButtonTheme: TextButtonThemeData(
-            style: TextButton.styleFrom(
-                foregroundColor: TobetoAppColor.textColor)));
+          style:
+              TextButton.styleFrom(foregroundColor: TobetoAppColor.textColor),
+        ),
+        iconButtonTheme: IconButtonThemeData(
+          style: IconButton.styleFrom(
+              foregroundColor: TobetoAppColor.textColor, iconSize: 25),
+        ),
+        iconTheme: IconThemeData(color: TobetoAppColor.textColor, size: 30));
   }
 
   static ThemeData darkTheme() {
     return ThemeData(
-        useMaterial3: true,
-        scaffoldBackgroundColor: TobetoAppColor.backgroundDark,
-        appBarTheme: const AppBarTheme(
-          systemOverlayStyle: SystemUiOverlayStyle.light,
+      useMaterial3: true,
+      scaffoldBackgroundColor: TobetoAppColor.backgroundDark,
+      appBarTheme: const AppBarTheme(
+        systemOverlayStyle: SystemUiOverlayStyle.light,
+      ),
+      colorScheme: TobetoAppColor.colorSchemeDark,
+      textTheme: GoogleFonts.poppinsTextTheme().copyWith(
+          labelLarge: GoogleFonts.poppins(
+              color: TobetoAppColor.textColor, fontWeight: FontWeight.bold),
+          displayLarge: TextStyle(color: TobetoAppColor.textColor),
+          bodyMedium: TextStyle(
+              color: TobetoAppColor.textColor, fontWeight: FontWeight.bold)),
+      inputDecorationTheme: InputDecorationTheme(
+        prefixIconColor: TobetoAppColor.textColor,
+        suffixIconColor: TobetoAppColor.textColor,
+        labelStyle: TextStyle(
+          color: TobetoAppColor.textColor,
         ),
-        colorScheme: TobetoAppColor.colorSchemeDark,
-        textTheme: GoogleFonts.poppinsTextTheme().copyWith(
-            labelLarge: GoogleFonts.poppins(
-                color: TobetoAppColor.textColor, fontWeight: FontWeight.bold),
-            displayLarge: TextStyle(color: TobetoAppColor.textColor),
-            bodyMedium: TextStyle(
-                color: TobetoAppColor.textColor, fontWeight: FontWeight.bold)),
-        inputDecorationTheme: InputDecorationTheme(
-          prefixIconColor: TobetoAppColor.textColor,
-          suffixIconColor: TobetoAppColor.textColor,
-          labelStyle: TextStyle(
-            color: TobetoAppColor.textColor,
-          ),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-              backgroundColor: TobetoAppColor.colorSchemeLight.primary,
-              fixedSize: const Size(350, 45),
-              foregroundColor: TobetoAppColor.textColorDark),
-        ),
-        textButtonTheme: TextButtonThemeData(
-            style: TextButton.styleFrom(
-                foregroundColor: TobetoAppColor.textColor)));
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+            backgroundColor: TobetoAppColor.colorSchemeLight.primary,
+            fixedSize: const Size(350, 45),
+            foregroundColor: TobetoAppColor.textColorDark),
+      ),
+      textButtonTheme: TextButtonThemeData(
+        style: TextButton.styleFrom(foregroundColor: TobetoAppColor.textColor),
+      ),
+      iconButtonTheme: IconButtonThemeData(
+        style: IconButton.styleFrom(
+            foregroundColor: TobetoAppColor.textColor, iconSize: 30),
+      ),
+    );
   }
 }
