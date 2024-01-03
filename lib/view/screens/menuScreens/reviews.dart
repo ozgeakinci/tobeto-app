f69e09e6626c9e11dcb6a46e21028078170a6162
ozge belen <ozgebelen@ozge-MacBook-Pro.local>
2024-01-04 01:28:41 +0300

    reviews edited

--- lib/view/screens/menuScreens/reviews.dart
+++ lib/view/screens/menuScreens/reviews.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:tobeto_app/theme/tobeto_theme_color.dart';
 
 class Reviews extends StatelessWidget {
   const Reviews({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Değerlendirmeler')),
-    );
+    return Scaffold(
+        body: ListView(
+      children: [
+        RichText(
+          text: TextSpan(children: [
+            TextSpan(
+                text: 'Yetkinlik',
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge
+                    ?.copyWith(color: ThemeData.light().primaryColor)),
+            TextSpan(
+                text: 'lerini ücretsiz ölç,',
+                style: TextStyle(color: TobetoAppColor.textColorBlack)),
+            const TextSpan(text: 'bilgi'),
+            const TextSpan(text: 'lerini test et.'),
+          ]),
+        )
+      ],
+    ));
   }
 }
