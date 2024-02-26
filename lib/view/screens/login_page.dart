8686d1b2542c0df0860979354a17722fb9a85ba2
ozge belen <ozgebelen@ozge-MacBook-Pro.local>
2024-02-27 00:36:18 +0300

    solve image-error

--- lib/view/screens/login_page.dart
+++ lib/view/screens/login_page.dart
@@ -64,30 +64,6 @@ class _LoginPageState extends State<LoginPage> {
                   prefixIcon: Icons.lock,
                   obscureText: true,
                   maxLines: 1,
-                   customValidator: (value) {
-                    if (!RegExp(r'[A-Z]').hasMatch(value)) {
-    return 'Şifre en az bir büyük harf içermelidir';
-  }
-
-  // Şifre en az bir küçük harf içermelidir
-  if (!RegExp(r'[a-z]').hasMatch(value)) {
-    return 'Şifre en az bir küçük harf içermelidir';
-  }
-
-  // Şifre en az bir rakam içermelidir
-  if (!RegExp(r'[0-9]').hasMatch(value)) {
-    return 'Şifre en az bir rakam içermelidir';
-  }
-
-  // Şifre en az bir özel karakter içermelidir
-  if (!RegExp(r'[!@#$%^&*(),.?":{}|<>]').hasMatch(value)) {
-    return 'Şifre en az bir özel karakter içermelidir';
-  }
-
-  // Şifre en az 8 karakterden oluşmalıdır
-  if (value.length < 8) {
-    return 'Şifre en az 8 karakterden oluşmalıdır';
-  }
                 ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.end,
