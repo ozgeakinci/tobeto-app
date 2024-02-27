8f9b4f8e54758055a51ca0d64e456218d5dbbc11
ozge belen <ozgebelen@ozge-MacBook-Pro.local>
2024-02-27 09:21:48 +0300

    fix some error (login error control code and splash image format)

--- lib/view/screens/splash_screen.dart
+++ lib/view/screens/splash_screen.dart
@@ -39,7 +39,7 @@ class _SplashScreenState extends State<SplashScreen> {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Image.asset("assets/images/tobeto_logo_w.png"),
+              Image.asset("assets/images/tobeto-logo_w.png"),
             ],
           ),
         ),
